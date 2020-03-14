875000920fb38fd6bc2fe4a36e02b813ac421d7f
Yu Cao <tsaoyu@tsaoyu.com>
2020-03-14 12:03:43 +0000

    Include cost func utility

---
+++ config/cost_func.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+Q = np.array([[1,0,0,0,0,0,0,0,0,0,0,0],          
+            [0,1,0,0,0,0,0,0,0,0,0,0],
+            [0,8,1,0,0,0,0,0,0,0,0,0],
+            [0,0,0,1,0,0,0,0,0,0,0,0], 
+            [0,0,0,0,1,0,0,0,0,0,0,0], 
+            [0,0,0,0,0,1,0,0,0,0,0,0],
+            [0,0,0,0,0,0,1,0,0,0,0,0], 
+            [0,0,0,0,0,0,0,1,0,0,0,0], 
+            [0,0,0,0,0,0,0,0,1,0,0,0],
+            [0,0,0,0,0,0,0,0,0,1,0,0], 
+            [0,0,0,0,0,0,0,0,0,0,1,0],
+            [0,0,0,0,0,0,0,0,0,0,0,1]])
+
+
+R = np.array([[1,0,0,0],
+              [0,1,0,0],
+              [0,0,1,0],
+              [0,0,0,1]])
+
+x = np.array([1.5,1,1,  1,   1,  1,  1,  1 ,1  , 1,   1,   1 ])
+
+def matrix_to_tuple_val(matrix):
+    col, row = matrix.shape
+    for c in range(col):
+        for r in range(row):
+            val = matrix[c,r]
+            if val != 0:
+                print('({},{}) {}'.format(c,r, val))
+
+    print('\n')
+
+def vector_to_tuple_val(vector):
+    col = len(vector)
+    for c in range(col):
+        val = vector[c]
+        if val != 0:
+            print('({},{}) {}'.format(c, 0, val))
+
+    print('\n')
+
+matrix_to_tuple_val(Q)
+matrix_to_tuple_val(R)
+vector_to_tuple_val(x)
